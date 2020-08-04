034aaf9db67a59ae332a2966b20d758d8710afaa
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-04 00:26:36 -0300

    Feat: add routes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Import assets
 import logoImg from '../../assets/images/logo.svg';
@@ -26,15 +27,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Icone de estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Icone de dar aulas" />
             Dar aulas
-          </a>
+          </Link>
 
         </div>
         <span className="total-connections">
