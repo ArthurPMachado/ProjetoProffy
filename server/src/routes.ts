ccfffa548294d463deb8ec1ffea3824785be2010
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-05 19:54:06 -0300

    Feat: list classes with filtering

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,65 +1,11 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
 
-interface ScheduleItem {
-  week_day: number,
-  from: string,
-  to: string
-}
+const classesControllers = new ClassesController();
 
-routes.post('/classes', async (request, response) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule,
-  } = request.body;
-
-  const transaction = await db.transaction();
-
-  try {
-    const insertedUsersIds = await transaction('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await transaction('classes').insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => ({
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinutes(scheduleItem.from),
-      to: convertHourToMinutes(scheduleItem.to),
-      class_id,
-    }));
-
-    await transaction('class_schedule').insert(classSchedule);
-
-    await transaction.commit();
-
-    return response.status(201).send();
-  } catch (error) {
-    await transaction.rollback();
-
-    return response.status(400).json({
-      error: 'Unexpected error while creating new class',
-    });
-  }
-});
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
 export default routes;
