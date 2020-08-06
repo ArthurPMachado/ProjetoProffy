b12ffb5366fb23a3576acb3ba0a2f3b90c39df3c
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-06 00:46:54 -0300

    Feat: add state in teacher form

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -5,14 +5,41 @@ import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
 
 import './styles.css';
+import Select from '../../components/Select';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
         <form id="search-teachers">
-          <Input name="subject" label="Matéria" />
-          <Input name="week_day" label="Dia da semana" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Java', label: 'Java' },
+              { value: 'Spring', label: 'Spring' },
+              { value: 'PHP', label: 'PHP' },
+              { value: 'JavaScript', label: 'JavaScript' },
+              { value: 'Scrum', label: 'Scrum' },
+              { value: '.NET', label: '.NET' },
+              { value: 'ReactJs', label: 'ReactJs' },
+              { value: 'React Native', label: 'React Native' },
+              { value: 'NodeJs', label: 'NodeJs' },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sabado' },
+            ]}
+          />
           <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
