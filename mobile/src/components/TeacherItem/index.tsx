47a4e24b9018cfd153232652f93736501a61ab36
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-07 17:02:23 -0300

    Feat: list teachers with api

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -8,49 +9,56 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
 
-function TeacherItem() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.profile}>
-        <Image
-          style={styles.avatar}
-          source={{ uri: 'https://github.com/ArthurPMachado.png' }}
-        />
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string
+}
 
-        <View style={styles.profileInfo}>
-          <Text style={styles.name}>Arthur Machado</Text>
-          <Text style={styles.subject}>Scrum</Text>
-        </View>
-      </View>
+interface TeacherItemProps {
+  teacher: Teacher
+}
 
-      <Text style={styles.bio}>
-        Entusiasta em organizar e automatizar processos através da tecnologia.
-        {'\n'}
-        {'\n'}
-        Desenvolvedor Web NodeJs, ReactJs, React-Native e Java
-      </Text>
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => (
+  <View style={styles.container}>
+    <View style={styles.profile}>
+      <Image
+        style={styles.avatar}
+        source={{ uri: teacher.avatar }}
+      />
 
-      <View style={styles.footer}>
-        <Text style={styles.price}>
-          Preço/hora
-          {' '}
-          {'   '}
-          <Text style={styles.priceValue}>R$ 60,00</Text>
-        </Text>
-
-        <View style={styles.buttonsContainer}>
-          <RectButton style={[styles.favoriteButton, styles.favorited]}>
-            <Image source={heartOutlineIcon} />
-          </RectButton>
-
-          <RectButton style={styles.contactButton}>
-            <Image source={whatsappIcon} />
-            <Text style={styles.contactButtonText}>Entrar em contato</Text>
-          </RectButton>
-        </View>
+      <View style={styles.profileInfo}>
+        <Text style={styles.name}>{teacher.name}</Text>
+        <Text style={styles.subject}>{teacher.subject}</Text>
       </View>
     </View>
-  );
-}
+
+    <Text style={styles.bio}>{teacher.bio}</Text>
+
+    <View style={styles.footer}>
+      <Text style={styles.price}>
+        Preço/hora
+        {' '}
+        {'   '}
+        <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton style={[styles.favoriteButton, styles.favorited]}>
+          <Image source={heartOutlineIcon} />
+        </RectButton>
+
+        <RectButton style={styles.contactButton}>
+          <Image source={whatsappIcon} />
+          <Text style={styles.contactButtonText}>Entrar em contato</Text>
+        </RectButton>
+      </View>
+    </View>
+  </View>
+);
 
 export default TeacherItem;
