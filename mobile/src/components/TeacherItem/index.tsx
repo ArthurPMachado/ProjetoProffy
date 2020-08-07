702f7b5ffd9e2f181c6b00fa7d633daab5d5d8ce
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-08-07 17:13:22 -0300

    Feat: add deep linking to whatsapp

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import {
+  View, Image, Text, Linking,
+} from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
@@ -23,42 +24,48 @@ interface TeacherItemProps {
   teacher: Teacher
 }
 
-const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => (
-  <View style={styles.container}>
-    <View style={styles.profile}>
-      <Image
-        style={styles.avatar}
-        source={{ uri: teacher.avatar }}
-      />
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
 
-      <View style={styles.profileInfo}>
-        <Text style={styles.name}>{teacher.name}</Text>
-        <Text style={styles.subject}>{teacher.subject}</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: teacher.avatar }}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>{teacher.bio}</Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora
+          {' '}
+          {'   '}
+          <Text style={styles.priceValue}>{teacher.cost}</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+
+          <RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
       </View>
     </View>
-
-    <Text style={styles.bio}>{teacher.bio}</Text>
-
-    <View style={styles.footer}>
-      <Text style={styles.price}>
-        Preço/hora
-        {' '}
-        {'   '}
-        <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
-      </Text>
-
-      <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.favoriteButton, styles.favorited]}>
-          <Image source={heartOutlineIcon} />
-        </RectButton>
-
-        <RectButton style={styles.contactButton}>
-          <Image source={whatsappIcon} />
-          <Text style={styles.contactButtonText}>Entrar em contato</Text>
-        </RectButton>
-      </View>
-    </View>
-  </View>
-);
+  );
+};
 
 export default TeacherItem;
